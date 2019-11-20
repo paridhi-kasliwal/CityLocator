bbf46a2c1ef63853479df91bc1b1ee35e3f00ead
Paridhi Kasliwal <paridhik1994@gmail.com>
2019-11-21 01:03:37 +0530

    Implementing google maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { StateService } from './service/state.service';
 import * as _ from 'underscore';
 
@@ -8,17 +8,65 @@ import * as _ from 'underscore';
 })
 
 export class AppComponent implements OnInit {
+  allData: any = [];
   stateList: any = [];
+  cityList: any = [];
+  currentState = 'Select State';
+  geocoder = new google.maps.Geocoder();
+
+  map: google.maps.Map;
+  @ViewChild('mapWrapper', {static: false}) mapElement: ElementRef;
+  
   constructor(private stateService: StateService) { }
 
   ngOnInit() {
     this.stateService.getStateList().subscribe((data) => {
-      console.log('data', data, typeof data);
-      this.stateList = data;
-    })
+      this.allData = data;
+
+      this.stateList = _.uniq(this.allData, function(d) {
+        return d['State'];
+      });
+    });
+  }
+
+  initializeMap() {
+    const lngLat = new google.maps.LatLng(6.5874964, 3.9886097);
+    const mapOptions: google.maps.MapOptions = {
+      center: lngLat,
+      zoom: 16,
+      fullscreenControl: false,
+      mapTypeControl: false,
+      streetViewControl: false
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
   }
 
   getSelectedState(state) {
-    console.log(state);
+    this.currentState = state;
+    this.stateService.getStateList(state).subscribe((data) => {
+      this.cityList = data;
+
+      _.each(this.cityList, (city) => {
+        this.codeAddress(city['City']);
+      });
+      
+      setTimeout(() => {
+        this.initializeMap();
+      }, 1000);
+    });
+  }
+
+  codeAddress(city) {
+    this.geocoder.geocode( { 'address': city}, function(results, status) {
+      if (status == 'OK') {
+        this.map.setCenter(results[0].geometry.location);
+        let marker = new google.maps.Marker({
+            map: this.map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
   }
 }
--- src/app/service/state.service.ts
+++ src/app/service/state.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class StateService {
   server_url = environment.server_url;
 
   constructor(private httpClient: HttpClient) { }
 
-  getStateList() {
-    return this.httpClient.get(this.server_url + 'cities');
+  getStateList(state?) {
+    let params;
+    if (state) {
+      params = new HttpParams()
+      .set('State', state);
+    }
+    return this.httpClient.get(this.server_url + 'cities', {params});
   }
 }
